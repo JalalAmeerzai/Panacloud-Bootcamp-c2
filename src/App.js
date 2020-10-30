3360af562d918d21d79bd92a7a7b5101ebedd331
Jalal <Jalalx@msn.com>
2020-10-30 19:17:59 +0500

    CLASS 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello World, React Base Application
         </p>
         <a
           className="App-link"
